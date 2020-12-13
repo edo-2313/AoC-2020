f55aa08d1efd58587831872ac25c7c176b09a80c
edo-2313 <32812884+edo-2313@users.noreply.github.com>
2020-12-13 16:15:57 +0100

    done day5

--- README.md
+++ README.md
@@ -9,7 +9,7 @@ Everything is run locally using Nodejs so I can take advantage of the *fs* modul
 - [x] Day 2
 - [x] Day 3
 - [x] Day 4
-- [ ] Day 5
+- [x] Day 5
 - [ ] Day 6
 - [ ] Day 7
 - [ ] Day 8
---
+++ day5/main.js
@@ -0,0 +1,42 @@
+const fs = require('fs');
+var array = fs.readFileSync('input.txt').toString().split("\n");
+
+//Part 1
+console.log("Part 1 result: ")
+
+var highest = 0;
+var seat_ids = [];
+array.forEach(element => {
+    row_start = 0;
+    row_end = 127;
+    element.substring(0, 7).split('').forEach(element => {
+        if (element == 'F')
+            row_end = Math.floor((row_end + row_start) / 2);
+        if (element == 'B')
+            row_start = Math.ceil((row_end + row_start) / 2);
+    });
+
+    col_start = 0;
+    col_end = 7;
+    element.substring(7).split('').forEach(element => {
+        if (element == 'L')
+            col_end = Math.floor((col_end + col_start) / 2);
+        if (element == 'R')
+            col_start = Math.ceil((col_end + col_start) / 2);
+    });
+    seat_ids.push(row_start * 8 + col_start);
+});
+console.log(Math.max(...seat_ids));
+
+//Part 2
+console.log("Part 2 result: ");
+
+for (let i = Math.min(...seat_ids); i < Math.max(...seat_ids); i++) {
+    if (!seat_ids.includes(i) && seat_ids.includes(i - 1) && seat_ids.includes(i + 1)){
+        console.log(i);
+        break;
+    }
+}
+
+
+//console.log(total);
